bb774360351ea6b53b2c28ac15377747a293abd8
Math-Fauch <faucher.mathieu@gmail.com>
2023-02-21 21:06:44 -0500

    amélioration de l'interface sans arduino

--- arduinoGUI.py
+++ arduinoGUI.py
@@ -2,43 +2,77 @@
 # GUI to turn ON blue/red LED with brightness control
 #-----------------------------------------------------
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, StringVar
 from pyfirmata import Arduino, PWM
 from time import sleep
-
-deuxiemeHarmoniqueBool = 0
-
+# global variables==================================================
+deuxiemeHarmoniqueBool = False
+flatDisplay = True
 # Functions=========================================================
-def blueLED():
-    delay = float(LEDtime.get())
-    brightness = float(LEDbright.get())
-    blueBtn.config(state = tk.DISABLED)
-    board.digital[3].write(brightness/100.0)
-    sleep(delay)
-    board.digital[3].write(0)
-    blueBtn.config(state = tk.ACTIVE)
-#--------------------------------------------
-def redLED():
-    delay = float(LEDtime.get())
-    brightness = float(LEDbright.get())
-    redBtn.config(state = tk.DISABLED)
-    board.digital[9].write(brightness/100.0)
-    sleep(delay)
-    board.digital[9].write(0)
-    redBtn.config(state = tk.ACTIVE)
-#--------------------------------------------
 def deuxiemeHarmonique():
     global deuxiemeHarmoniqueBool
     if deuxiemeHarmoniqueBool:
-        LEDbright.configure(from_=90, to=120)
+        frequency.configure(from_=90.00, to=120.00)
+        frequency.set(90.00)
+        freqText.set("fréquence première harmonique")
         deuxiemeHarmoniqueBool = False
     else:
-        LEDbright.configure(from_=180, to=240)
+        frequency.configure(from_=180.00, to=240.00)
+        frequency.set(180.00)
+        freqText.set("fréquence deuxième harmonique")
         deuxiemeHarmoniqueBool = True
 #--------------------------------------------
+def changeFlat():
+    global flatDisplay
+    if flatDisplay:
+        FSharpBtn.configure(text="G♭")
+        gSharpBtn.configure(text="A♭")
+        aSharpBtn.configure(text="B♭")
+        flatDisplay = False
+    else:
+        FSharpBtn.configure(text="F♯")
+        gSharpBtn.configure(text="G♯")
+        aSharpBtn.configure(text="A♯")
+        flatDisplay = True
+#--------------------------------------------
+def fSharp():
+    global deuxiemeHarmoniqueBool
+    if deuxiemeHarmoniqueBool:
+        frequency.set(185.00)
+    else:
+        frequency.set(92.50)
+#--------------------------------------------
+def g():
+    global deuxiemeHarmoniqueBool
+    if deuxiemeHarmoniqueBool:
+        frequency.set(196.00)
+    else:
+        frequency.set(98.00)
+#--------------------------------------------
+def gSharp():
+    global deuxiemeHarmoniqueBool
+    if deuxiemeHarmoniqueBool:
+        frequency.set(207.65)
+    else:
+        frequency.set(103.83)
+#--------------------------------------------
+def a():
+    global deuxiemeHarmoniqueBool
+    if deuxiemeHarmoniqueBool:
+        frequency.set(220.00)
+    else:
+        frequency.set(110.00)
+#--------------------------------------------
+def aSharp():
+    global deuxiemeHarmoniqueBool
+    if deuxiemeHarmoniqueBool:
+        frequency.set(233.08)
+    else:
+        frequency.set(116.54)
+#--------------------------------------------
 def aboutMsg():
-    messagebox.showinfo("About",
-    "Logic Don't Care Software\nLED Control Ver 1.0\nAugust 2022")
+    messagebox.showinfo("À propos",
+    "Projet de Design II(Modélisation), Université Laval, H23")
 #=================================================================
 # Arduino board connected to serial port COM3
 board = Arduino("COM3")
@@ -48,32 +82,43 @@ board.digital[9].mode = PWM
 #--------------------------------------------
 # initialize window with title & size
 win = tk.Tk()
-win.title("LED Control")
+win.title("Archet électronique")
 win.minsize(235,150)
-#--------------------------------------------
-# Entry widget
-LEDtime = tk.Entry(win, bd=6, width=8)
-LEDtime.grid(column=1, row=1)
-# Label widget
-tk.Label(win, text="LED ON Time (sec)").grid(column=2, row=1)
+freqText = StringVar()
+freqText.set("fréquence première harmonique")
 #--------------------------------------------
 # Scale widget
-LEDbright = tk.Scale(win, bd=5, from_=90, to=120, orient=tk.HORIZONTAL)
-LEDbright.grid(column=1, row=2)
+frequency = tk.Scale(win, bd=5, from_=90.00, to=120.00, orient=tk.HORIZONTAL)
+frequency.grid(column=3, row=2)
 # Label widget
-tk.Label(win, text="fréquence").grid(column=2, row=2)
+tk.Label(win, textvariable=freqText).grid(column=3, row=1)
 #--------------------------------------------
 # Button widgets
-blueBtn = tk.Button(win, bd=5, bg='#89CFF0', text="Harmo", command=deuxiemeHarmonique)
-blueBtn.grid(column=1, row=3)
+harmoniqueBtn = tk.Button(win, bd=5, width=6 , bg='#89CFF0', text="harmo", command=deuxiemeHarmonique)
+harmoniqueBtn.grid(column=2, row=3)
 
-redBtn = tk.Button(win, bd=5, bg='red', text="LED ON", command=redLED)
-redBtn.grid(column=2, row=3)
+flatSharpBtn = tk.Button(win, bd=5, width=6 ,bg='#89CFF0', text="♯/♭", command=changeFlat)
+flatSharpBtn.grid(column=4, row=3)
 
-aboutBtn = tk.Button(win, text="About", command=aboutMsg)
-aboutBtn.grid(column=1, row=4)
+FSharpBtn = tk.Button(win, bd=5, width=6, bg='yellow', text="F♯", command=fSharp)
+FSharpBtn.grid(column=1, row=4)
 
-quitBtn = tk.Button(win, text="Quit", command=win.quit)
-quitBtn.grid(column=2, row=4)
+gBtn = tk.Button(win, bd=5, width=6, bg='yellow', text="G", command=g)
+gBtn.grid(column=2, row=4)
+
+gSharpBtn = tk.Button(win, bd=5, width=6, bg='yellow', text="G♯", command=gSharp)
+gSharpBtn.grid(column=3, row=4)
+
+aBtn = tk.Button(win, bd=5, width=6, bg='yellow', text="A", command=a)
+aBtn.grid(column=4, row=4)
+
+aSharpBtn = tk.Button(win, bd=5, width=6, bg='yellow', text="A♯", command=aSharp)
+aSharpBtn.grid(column=5, row=4)
+
+aboutBtn = tk.Button(win, width=6, text="À propos", command=aboutMsg)
+aboutBtn.grid(column=2, row=5)
+
+quitBtn = tk.Button(win, width=6, text="Quit", command=win.quit)
+quitBtn.grid(column=4, row=5)
 #--------------------------------------------
 win.mainloop()
